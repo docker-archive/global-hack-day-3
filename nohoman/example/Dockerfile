d95c0a82c06ddb1e39af898f579d9578807c749e
vagrant <vagrant@localhost.localdomain>
2015-09-19 22:01:01 +1000

    Example files

---
+++ nohoman/example/Dockerfile
@@ -0,0 +1,3 @@
+FROM tomcat:8.0
+
+ADD files/sample.war $CATALINA_HOME/webapps/
---
+++ nohoman/example/files/mimic-change.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp sample.war sample.war.bck
+mv sample-patch.war sample.war
+mv sample.war.bck sample-patch.war
