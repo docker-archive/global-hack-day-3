5635e521db130e925b8393d09ffa5da78a4c7f44
shaded-enmity <the.arul@gmail.com>
2015-09-19 21:04:50 +0200

    New bind home/pwd injectors

---
+++ injectors/bind_home.py
@@ -0,0 +1,20 @@
+# vim: set fileencoding=utf-8
+# Pavel Odvody <podvody@redhat.com>
+#
+# HICA - Host integrated container applications
+#
+# MIT License (C) 2015
+
+import os
+from base.hica_base import *
+
+class BindHomeInjector(HicaInjector):
+  def get_config_key(self):
+    return "io.hica.bind_home"
+
+  def get_injected_args(self):
+    return (("--home-path", HicaValueType.PATH, os.getenv("HOME")),)
+
+def register(context):
+  obj = BindHomeInjector()
+  context[obj.get_config_key()] = obj
---
+++ injectors/bind_pwd.py
@@ -0,0 +1,20 @@
+# vim: set fileencoding=utf-8
+# Pavel Odvody <podvody@redhat.com>
+#
+# HICA - Host integrated container applications
+#
+# MIT License (C) 2015
+
+import os
+from base.hica_base import *
+
+class BindPwdInjector(HicaInjector):
+  def get_config_key(self):
+    return "io.hica.bind_pwd"
+
+  def get_injected_args(self):
+    return ((None, HicaValueType.PATH, os.getenv("PWD")),)
+
+def register(context):
+  obj = BindPwdInjector()
+  context[obj.get_config_key()] = obj
